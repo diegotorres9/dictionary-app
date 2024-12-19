3b84404110e6176fb3da5c53c4a872a95bef1c1c
Diego Torres <diegoetorres1@gmail.com>
2024-12-18 21:27:30 -0500

    moved search function from searchbar component to parent

--- app/components/searchbar/Searchbar.js
+++ app/components/searchbar/Searchbar.js
@@ -1,12 +1,15 @@
+
 import Image from "next/image";
 import SearchIcon from "../../../public/images/icon-search.svg";
 import styles from "./searchbar.module.css"
 
-export default function Searchbar () {
+
+export default function Searchbar ({handleSearch, fetchData}) {
+
     return (
-        <div className="search-bar-container">
-            <form action="" className={styles.form}> 
-                <input aria-label="search" type="search" id="searchbar" className={styles.searchbar} name="searchbar" />
+        <section className="search-bar-container">
+            <form action="" className={styles.form} onSubmit={fetchData}> 
+                <input aria-label="search" type="search" id="searchbar" className={styles.searchbar} name="searchbar" onChange={handleSearch}/>
                 <button type="submit" className={`${styles.search} ${styles.button}`}>
                     <Image 
                         width={15.55}
@@ -16,6 +19,6 @@ export default function Searchbar () {
                     />
                 </button>
             </form> 
-        </div>
+        </section>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,13 +1,38 @@
-import Header from "./components/header/Header";
+"use client";
+
+import { useState } from "react";
 import Searchbar from "./components/searchbar/Searchbar";
+import DefinitionContainer from "./components/defnitions/DefinitionContainer";
+
 
 export default function Page() {
+    const [inputValue, setInputValue] = useState('');
+    const [data, setData] = useState('');
+
+
+    const handleSearch = (e) => {
+        setInputValue(e.target.value);
+    };
+
+    const fetchData = async (event) => {
+        event.preventDefault();
+        try {
+            const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputValue}`);
+            const data = await response.json()
+            setData(data);
+            // console.log(JSON.stringify(data, null, 2));
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
     return (
-        <>
-        <Header />
         <main>
-            <Searchbar />
+            <Searchbar 
+                handleSearch={handleSearch}
+                fetchData={fetchData}
+            />
+            <DefinitionContainer data={data}/>
         </main>
-        </>
     )
 }

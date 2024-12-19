24d7d8cb29460abd0e4fbb340514dcc307b2fda6
Diego Torres <diegoetorres1@gmail.com>
2024-12-18 21:26:43 -0500

    moved header component to layout file

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,11 @@
+import Header from "./components/header/Header"
 export default function RootLayout({ children }) {
     return (
     <html lang="en">
-        <body>{children}</body>
+        <body>
+            <Header />
+            {children}
+        </body>
     </html>
     )
 }

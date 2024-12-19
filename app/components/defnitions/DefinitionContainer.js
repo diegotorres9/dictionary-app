38ccb68a221b0d857fefd884386adefd812e62a9
Diego Torres <diegoetorres1@gmail.com>
2024-12-18 21:27:48 -0500

    new component to render data from api call

---
+++ app/components/defnitions/DefinitionContainer.js
@@ -0,0 +1,9 @@
+export default function DefinitionContainer ({data}) {
+    return (
+        <section>
+        {/* {data && <pre>{JSON.stringify(data, null, 2)}</pre>} */}
+        <p>RESULTS HEEREE</p>
+        {console.log(data)}
+        </section>
+    )
+}

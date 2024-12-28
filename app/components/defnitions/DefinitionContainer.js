e09a9cb858b28d43b2c9ce4c1bc34190123192ac
Diego Torres <diegoetorres1@gmail.com>
2024-12-27 21:18:03 -0500

    rremoved json.stringify call

--- app/components/defnitions/DefinitionContainer.js
+++ app/components/defnitions/DefinitionContainer.js
@@ -6,7 +6,6 @@ export default function DefinitionContainer ({data}) {
     }, [data])
     return (
         <section className="results-container">
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>} 
         {data.map((result, index) => (
             <div key={index}>
                 <div>

3e3bdd967b38034a2d71854249eab79f6b1541b5
Diego Torres <diegoetorres1@gmail.com>
2024-12-27 20:17:32 -0500

    data map renders out fetch data to component

--- app/components/defnitions/DefinitionContainer.js
+++ app/components/defnitions/DefinitionContainer.js
@@ -1,7 +1,31 @@
+import { useEffect } from "react"
 export default function DefinitionContainer ({data}) {
+    useEffect(() => {
+        console.log(data);
+        // let results = data;
+    }, [data])
     return (
         <section className="results-container">
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>} 
+        {data.map((result, index) => (
+            <div key={index}>
+                <div>
+                    <h1>{result.word}</h1>
+                    <span>{result.phonetic}</span>
+                </div>
+                {result.meanings.map((speech, index) => (
+                    <div key={index}>
+                        <span>{speech.partOfSpeech}</span>
+                        <p>Meaning</p>
+                        {speech.definitions.map((definition, index) => (
+                            <ul key={index}>
+                                <li>{definition.definition}</li>
+                            </ul>
+                        ))}
+                    </div>
+                ))}
+            </div>
+        ))}
         </section>
     )
 }

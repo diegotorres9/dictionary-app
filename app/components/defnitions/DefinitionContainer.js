1cfb3054a49091a1cddb01205b3714f870417c1f
Diego Torres <diegoetorres1@gmail.com>
2024-12-30 13:28:04 -0500

    map to get word pronunciation and button click to play sound

--- app/components/defnitions/DefinitionContainer.js
+++ app/components/defnitions/DefinitionContainer.js
@@ -1,4 +1,7 @@
 import { useEffect } from "react"
+import Image from "next/image";
+import PlayButton from "../../../public/images/icon-play.svg";
+
 export default function DefinitionContainer ({data}) {
     useEffect(() => {
         console.log(data);
@@ -6,11 +9,28 @@ export default function DefinitionContainer ({data}) {
     }, [data])
     return (
         <section className="results-container">
+        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
         {data.map((result, index) => (
             <div key={index}>
                 <div>
                     <h1>{result.word}</h1>
                     <span>{result.phonetic}</span>
+                    {result.phonetics.map((sound, ind) => {
+                        const pronounce = sound.audio;
+                        const audio = new Audio(pronounce);
+                        console.log(sound.audio);
+                        return (
+                            <button key={ind} onClick={() => audio.play()}>
+                                <Image 
+                                    width={75}
+                                    height={75}
+                                    alt=""
+                                    src={PlayButton}
+                                />
+                            </button>
+                        )
+                    })}
+
                 </div>
                 {result.meanings.map((meaning, idx) => (
                     <div key={idx}>
@@ -18,7 +38,7 @@ export default function DefinitionContainer ({data}) {
                         <p>Meaning</p>
                         {meaning.definitions.map((definition, i) => (
                             <div key={i}>
-                                <ul >
+                                <ul>
                                     <li>{definition.definition}</li>
                                 </ul>
                             </div>

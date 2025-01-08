dba466fd6ed695951656ef2e7ce89ef7503205d2
Diego Torres <diegoetorres1@gmail.com>
2025-01-08 14:02:51 -0500

    mapping data

--- app/components/defnitions/DefinitionContainer.js
+++ app/components/defnitions/DefinitionContainer.js
@@ -4,42 +4,66 @@ import PlayButton from "../../../public/images/icon-play.svg";
 
 export default function DefinitionContainer ({data}) {
     useEffect(() => {
-        console.log(data);
-        // let results = data;
     }, [data]);
     return (
         <section className="results-container">
-        {data && <pre>{JSON.stringify(data, null, 2)}</pre>}
-        {data.map((result, index) => (
-            <div key={index}>
+        {/* {data && <pre>{JSON.stringify(data, null, 2)}</pre>} */}
+        {data.map((result, index) => {
+            const {word, phonetic, sourceUrls, meanings, phonetics} = result;
+
+            const sounds = Object.values(phonetics).filter(sounds => sounds.audio !== "");
+
+            return (
+            <div className="definition-wrapper">
                 <div>
-                    <h1>{result.word}</h1>
-                    <span>{result.phonetic}</span>
+                    <h1>{word}</h1>
+                    <span>{phonetic}</span>
+                    {sounds.map((sound) => {
+                    // console.log(sound.audio);
+                    const pronounce = sound.audio;
+                    const audio = new Audio(pronounce);
+                    return (
+                        <button onClick={() => audio.play()}>
+                            <Image 
+                                width={78}
+                                height={78}
+                                alt=""
+                                src={PlayButton}
+                            />
+                        </button>
+                    )
+                    })}
 
                 </div>
-                {result.meanings.map((meaning, idx) => (
-                    <div key={idx}>
-                        <span>{meaning.partOfSpeech}</span>
-                        <p>Meaning</p>
-                        {meaning.definitions.map((definition, i) => (
-                            <div key={i}>
+                <div>
+                    {meanings.map((meaning) => (
+                        <div>
+                            <span>{meaning.partOfSpeech}</span>
+                            <p>Meaning</p>
+                            <ul>
+                                {meaning.definitions.map((define) => (
+                                    <li>{define.definition}</li>
+                                ))}
+                            </ul>
+                            <div>
+                                <p>{meaning.synonyms.length === 0 ? '' : 'Synonymns'}</p>
                                 <ul>
-                                    <li>{definition.definition}</li>
+                                    {meaning.synonyms.map((synonym) => (
+                                        <li>{synonym.length === 0 ? '' : synonym}</li>
+                                    ))}
                                 </ul>
                             </div>
-                        ))}
-                        {meaning.synonyms.length == 0 ? '' : <p>Synonyms</p>}
-                        {meaning.synonyms.map((synonym, indx) => (
-                            <div key={indx}>
-                                <ul>
-                                    <li>{synonym}</li>
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
-                ))}
+                        </div>
+                    ))}
+                </div>
+                <div className="source-wrapper">
+                    <p>Source</p>
+                    <a href={sourceUrls[0]}>{sourceUrls[0]}</a>
+                </div>
             </div>
-        ))}
+            )
+
+        })}
         </section>
     )
 }

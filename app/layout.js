8376fd4ad2d58ab31f5b78691e2044073bb4ad19
Diego Torres <diegoetorres1@gmail.com>
2025-01-14 15:41:35 -0500

    created font change function and passed into Header component

--- app/layout.js
+++ app/layout.js
@@ -1,10 +1,18 @@
+"use client";
+import { useState } from "react";
 import Header from "./components/header/Header";
 import './styles/globals.css';
 export default function RootLayout({ children }) {
+    const [selectFont, setSelectFont] = useState('');
+
+    const fontChange = (e) => {
+        setSelectFont(e.target.value);
+    }
+
     return (
-    <html lang="en">
-        <body className="px-6 pt-6 pb-auto lg:px-xl-x lg:pt-xl-t lg:pb-xl-b">
-            <Header />
+    <html lang="en" suppressHydrationWarning>
+        <body className={`px-6 pt-6 pb-auto lg:px-xl-x lg:pt-xl-t lg:pb-xl-b ${selectFont}`}>
+            <Header fontChange={fontChange} />
             {children}
         </body>
     </html>

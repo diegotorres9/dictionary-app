bf920572858c16b6c1ccdefdddfba15b21d94d96
Diego Torres <diegoetorres1@gmail.com>
2025-01-13 22:13:27 -0500

    tailwind classes for body padding

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import './styles/globals.css';
 export default function RootLayout({ children }) {
     return (
     <html lang="en">
-        <body>
+        <body className="px-6 pt-6 pb-auto lg:px-xl-x lg:pt-xl-t lg:pb-xl-b">
             <Header />
             {children}
         </body>

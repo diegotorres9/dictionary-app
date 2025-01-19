e5f00f756f4d9c612fed1a35872b514e372daaf6
Diego Torres <diegoetorres1@gmail.com>
2025-01-18 21:32:51 -0500

    change fontchange function to target id instead of value; added dark class for dark mode

--- app/layout.js
+++ app/layout.js
@@ -7,11 +7,11 @@ export default function RootLayout({ children }) {
     const [selectFont, setSelectFont] = useState('');
 
     const fontChange = (e) => {
-        setSelectFont(e.target.value);
+        setSelectFont(e.target.id);
     }
 
     return (
-    <html lang="en" suppressHydrationWarning={true}>
+    <html lang="en" className="dark" suppressHydrationWarning={true}>
         <body className={`px-6 pt-6 pb-auto lg:px-xl-x lg:pt-xl-t lg:pb-xl-b ${selectFont}`}>
             <Theme>
                 <Header fontChange={fontChange} />

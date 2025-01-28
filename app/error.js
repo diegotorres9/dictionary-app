da50c2e95c1d1a007417d265cdab8b846ff5857f
Diego Torres <diegoetorres1@gmail.com>
2025-01-28 09:39:11 -0500

    error message class name

--- app/error.js
+++ app/error.js
@@ -1,11 +1,10 @@
 'use client';
 
-
-const error = ({}) => {
-    return (
+const error = ({error}) => {
+        return (
         <div className="flex flex-col items-center justify-center text-center">
             😕 
-            <p>No Definitions Found</p>
+            <p className="mt-[44px] mb-6">No Definitions Found</p>
             <p>Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead.</p>
         </div>
     )
